b73df456734d1ee0b92d40702125d875b1f09e4b
YooHyeok <yjk7454@naver.com>
2023-06-21 15:43:02 +0900

    lombok 테스트

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 롬복 테스트
+ */
+
+@Getter @Setter
+public class Hello {
+    private String testData;
+}
--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setTestData("hello");
+		String testData = hello.getTestData();
+		System.out.println("testData = " + testData);
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
