6ffdab84b68aa5515ef68596573c4792a0011780
YooHyeok <yjk7454@naver.com>
2023-06-22 10:50:30 +0900

    Address Setter를 Constructor로 대체

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.setTestData("hello");
-		String testData = hello.getTestData();
-		System.out.println("testData = " + testData);
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() { //projected 이유 : JPA스펙상 만들어놓은거구나! 함부로 new로 생성하면 안되겠구나!
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
