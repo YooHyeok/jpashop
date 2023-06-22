b2ec87b8ac31b70e3b251988b62a8cff67579ac8
YooHyeok <yjk7454@naver.com>
2023-06-22 14:27:57 +0900

    MemberService 회원 서비스 추가

---
+++ src/main/java/jpabook/jpashop/domain/service/MemberService.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 읽기전용(select) - 더티체킹X, DB리소스절약 등의 이점
+public class MemberService {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional(readOnly = false)// 기본값 false (insert,update 등 변경사항)
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복회원 검증 메소드
+     * @param member
+     */
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName()); // 동시성 문제 대안 : name 컬럼 Unique 제약조건 지정
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원 입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 단건 조회
+     */
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+}
