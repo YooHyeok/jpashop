664057195276eb71fa4f1f56336a03c61ccf5391
YooHyeok <yjk7454@naver.com>
2023-06-22 23:06:22 +0900

    주문 레포지토리 검색조건 기능 추가 (JPA) 및 주문검색 클래스 추가

--- src/main/java/jpabook/jpashop/domain/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/domain/repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package jpabook.jpashop.domain.repository;
 
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 
 @Repository
@@ -27,9 +32,49 @@ public class OrderRepository {
     }
 
     /**
-     * [주문 전체조회]
+     * [주문 전체조회] <br/>
+     * 동적쿼리 - JPQL
      */
-    /*public List<Order> findAll(OrderSearch orderSearch) {
-        return em.createQuery("select o from Order o", Order.class).getResultList();
-    }*/
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true; //조건여부 true
+
+        String memberName = orderSearch.getMemberName();
+        OrderStatus orderStatus = orderSearch.getOrderStatus();
+
+        // 주문 상태 검색
+        if (orderStatus != null) { // 검색조건 중 주문상태가 존재할 경우 where
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false; // 조건여부를 false로 변경 후 종료
+            } else {  // 조건여부 false이면 and
+                jpql += " and";
+            }
+            jpql += "o.status = :status"; // 조건 추가
+        }
+
+        // 회원 이름 검색
+
+        if (memberName != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += "m.name like :name"; // 조건 추가
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);//최대 1000건
+
+        if (orderStatus != null) { // orderStatus 값이 존재하면 쿼리 파라미터 바인딩
+            query = query.setParameter("status", orderStatus);
+        }
+        if (StringUtils.hasText(memberName)) { // StringUtils 사용 (ValueArgs에 String값이 포함되어있는지)
+            query = query.setParameter("name", memberName);
+        }
+        return query.getResultList();
+    }
+
 }
---
+++ src/main/java/jpabook/jpashop/domain/repository/OrderSearch.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.domain.repository;
+
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 검색조건 파라미터 클래스 <br/>
+ * 회원 이름과 주문 상태를 기준으로 검색하는 클래스이다.
+ */
+@Getter @Setter
+public class OrderSearch {
+    private String memberName; //회원 이름
+    private OrderStatus orderStatus; //주문 상태[ORDER, CANCEL]
+}
