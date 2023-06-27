4125b8a21386f3a325643b1a650ab1eaa0162adb
YooHyeok <yjk7454@naver.com>
2023-06-27 09:28:57 +0900

    JPA활용2 - API 개발과 성능 최적화 시작 - MemberRestController추가

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+}
