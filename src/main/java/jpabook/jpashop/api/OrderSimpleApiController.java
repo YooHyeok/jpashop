5c84957c838b5fc8c19aa6d768e511b377d776f0
YooHyeok <yjk7454@naver.com>
2023-06-27 14:37:24 +0900

    주문조회 API V2 - 엔터티 Dto로 변환(OrderDto/OrderItemDto)

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,20 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Order 조회 (xToOne 관계 최적화)
@@ -42,4 +49,63 @@ public class OrderSimpleApiController {
         }
         return all;
     }
+
+    /**
+     * 지연 로딩으로 너무 많은 SQL을 실행한다. (N+1) <br/>
+     * Order 2개 (1번) 1 +2+2+2
+     *  -> Member1, Delivery(address), OrderItem, Item <br/>
+     *  -> Member2, Delivery(address), OrderItem, Item <br/>
+     *
+     * @return
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(order -> new OrderDto(order))// OrderDto로 반복 변환
+                .collect(Collectors.toList()); // 변환한 Dto 객체들을 List로 변환
+        return result;
+
+    }
+
+    /**
+     * Order에 대한 Dto변환
+     */
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime localDateTime;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName(); // Lazy 초기화 (영속성컨텍스트 검색후 없으면 DB쿼리 호출)
+            this.localDateTime = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress(); // Lazy 초기화 (영속성컨텍스트 검색후 없으면 DB쿼리 호출)
+            this.orderItems = order.getOrderItems().stream()// Lazy 초기화 (영속성컨텍스트 검색후 없으면 DB쿼리 호출)
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    /**
+     * OrderItem에 대한 Dto 변환
+     */
+    @Data
+    static class OrderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int  count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getOrderPrice();
+            this.count = orderItem.getCount();
+        }
+    }
+
 }
