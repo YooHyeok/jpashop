e252a79de4631f4d3eda57aa3e711f9c1081fa50
YooHyeok <yjk7454@naver.com>
2023-06-27 09:49:00 +0900

    회원 등록 API version1

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,12 +1,47 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
+
+    /**
+     * 등록 V1 : 요청 값으로 Member 엔티티를 직접 받는다. <br/>
+     * 문제점
+     * - 엔티티에 프레젠티에션 계층을 위한 로직이 추가된다.
+     * - 엔티티에 API 검증을 위한 로직이 들어간다. (@NotEmpty 등등)
+     * - 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위한 모든 요청 요구사항을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변하낟.
+     * 결론
+     * - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
+     * @param member {"name":"hello"} (Body - JSON)
+     * @return
+     */
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+
+
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
 
 }
