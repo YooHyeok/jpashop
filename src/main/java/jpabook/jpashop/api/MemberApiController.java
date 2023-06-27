fd1dd42df9f57bb5226e8ac204665b6f35943750
YooHyeok <yjk7454@naver.com>
2023-06-27 10:34:35 +0900

    회원 조회 API version1

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -54,6 +55,22 @@ public class MemberApiController {
         memberService.update(id, request.getName());
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    /**
+     * 회원 조회 API v1 - 응답값으로 엔터티를 외부에 직접 노출 <br/>
+     * 문제점 <br/>
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다. <br/>
+     * - 기본적으로 엔티티의 모든 값이 노출된다. <br/>
+     * - 응답 스펙을 맞추기 위해 로직이 추가된다.(@JsonIgnore, 별도의 뷰 로직 등등) <br/>
+     * - 실무에서는 같은 엔티티에 대해 API가 용도에 따라 다양하게 만들어지는데, <br/>
+     *   한 엔티티에 각각의 API를 위한 프레젠테이션 응답 로직을 담기는 어렵다. <br/>
+     * - 엔티티가 변경되면 API 스펙이 변한다. <br/>
+     * - 추가로 컬렉션을 직접 반환하면 향후 API 스펙을 변경하기 어렵다.(별도의 Result 클래스 생성으로 해결)
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
     }
 
     /**
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,6 +20,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    @JsonIgnore // Member를 조회할때 Member에 대한 엔터티만 조회할 수 있도록 무시한다.
     @OneToMany(mappedBy = "member")// 연관관계 거울:읽기전용 (Order에 있는 member필드와 매핑) _ 양방향 연관관계 - 여러개의 주문은 각각 하나의 member를 갖는다.
     private List<Order> orders = new ArrayList<>(); //값을 넣는다고 해서 fk값이 변경되지 않는다.
 
